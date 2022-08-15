0369dfd64858adf59c290c9b559e261b5e619a6f
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2022-08-15 13:06:35 +0200

    fix: dicing

--- islands/dice.tsx
+++ islands/dice.tsx
@@ -6,27 +6,33 @@ import { Button } from '../components/button.tsx'
 
 export default function Dice() {
   const [diceNumber, setDiceNumber] = useState<null | number>()
-  const [isDicing, setIsDicing] = useState(false)
+  const [dicingCounter, setDicingCounter] = useState(0)
+  const [hasTriggeredDicing, setHasTriggeredDicing] = useState(false)
   const [isForceUpdate, setIsForceUpdate] = useState(false)
 
   useEffect(() => {
-    if (isDicing) {
+    if (hasTriggeredDicing) {
       setTimeout(() => {
-        setDiceNumber(Math.ceil(Math.random() * 6))
-        setIsForceUpdate((prev) => !prev)
+        if (dicingCounter <= 9) {
+          setDiceNumber(Math.ceil(Math.random() * 6))
+          setIsForceUpdate((prev) => !prev)
+          setDicingCounter((prev) => prev + 1)
+        } else {
+          setHasTriggeredDicing(false)
+          setDicingCounter(0)
+        }
       }, 300)
-      setTimeout(() => {
-        setIsDicing(false)
-      }, 3000)
     }
-  }, [isDicing, diceNumber, isForceUpdate])
+  }, [diceNumber, dicingCounter, hasTriggeredDicing, isForceUpdate])
+
+  const isDicing = dicingCounter > 0
 
   return (
     <div class="dice">
       <p class="number" style={isDicing ? 'opacity: 0.5' : undefined}>
         {diceNumber ?? '_'}
       </p>
-      <Button disabled={isDicing} onClick={() => setIsDicing(true)}>
+      <Button disabled={isDicing} onClick={() => setHasTriggeredDicing(true)}>
         Würfeln
       </Button>
     </div>
