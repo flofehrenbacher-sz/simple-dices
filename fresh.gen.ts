01d516c9c41d948921c731c6f1e1c9d8ed2f5cea
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2022-08-15 11:32:29 +0200

    feat: github user page (fresh getting started)

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,8 @@ import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/random-uuid.ts";
-import * as $4 from "./routes/index.tsx";
+import * as $4 from "./routes/github/[username].tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,7 +16,8 @@ const manifest = {
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/api/random-uuid.ts": $3,
-    "./routes/index.tsx": $4,
+    "./routes/github/[username].tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
